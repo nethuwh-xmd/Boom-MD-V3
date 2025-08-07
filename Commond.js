9154db3d01c5860b81478825e7474f62de48c10b
nethuwh-xmd <nethumjayarathna8@gmail.com>
2025-08-07 16:47:24 +0530

    Create Commond.js

---
+++ Commond.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    command: 'alive',
+    description: 'Check bot status',
+    category: 'General',
+  },
+  {
+    command: 'menu',
+    description: 'Display all commands',
+    category: 'General',
+  },
+  {
+    command: 'ping',
+    description: 'Check speed',
+    category: 'General',
+  },
+  {
+    command: 'song',
+    description: 'Download songs by name',
+    category: 'Downloader',
+  },
+  {
+    command: 'movie',
+    description: 'Download real movies by title',
+    category: 'Downloader',
+  },
+  {
+    command: 'help',
+    description: 'Show help menu',
+    category: 'General',
+  },
+];
