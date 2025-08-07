9cbe2aa150ea90a6ffbf131970afa55431d4924d
nethuwh-xmd <nethumjayarathna8@gmail.com>
2025-08-07 16:46:37 +0530

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  owner: ['94712345678'], // ඔබේ ගිණුම් අංකය
+  botName: 'BOOM-MD-V3',
+  prefix: '.', // ඔබට කැමති prefix එක
+  aliveMsg: '💥 BOOM-MD-V3 is Alive!',
+  menuMsg: '📜 BOOM-MD-V3 Command List',
+  songAPI: 'https://api.nethu.live/song?s=',   // Song download API (example)
+  movieAPI: 'https://api.nethu.live/movie?q='  // Movie fetch API (example)
+};
