32fb0ef244ebf9248f327c432e9e05de3de84a6d
nethuwh-xmd <nethumjayarathna8@gmail.com>
2025-08-07 17:03:32 +0530

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,5 +1,5 @@
 module.exports = {
-  sessionFolder: 'session',
+  sessionFolder: 'ZORA~ackXSTIZ#Ux0nv-2MFxbnNiVZAuRUOveu55N0DecTRF5d95yiSY4',
   logo: 'https://files.catbox.moe/fge8q7.jpg', // Connected image
   menuImage: 'https://files.catbox.moe/4ior1g.jpg', // Menu image
   aliveImage: 'https://files.catbox.moe/pgcxfy.jpg',
